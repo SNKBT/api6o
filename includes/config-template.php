3f94508a77b6972d12cc5d8bbc27fb382e989507
SNKBT <snkbt@erenya.ch>
2014-05-19 13:26:33 +0200

    Example configuration

---
+++ includes/config-template.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Database configuration
+ */
+define ( 'DB_USERNAME', 'please replace username' );
+define ( 'DB_PASSWORD', 'please replace password' );
+define ( 'DB_HOST', 'please replace db host' );
+define ( 'DB_NAME', 'please replace db name' );
+
+define ( 'SERVER_ROOT', '/' );
+define ( 'DEBUG', false );
+
+?>
